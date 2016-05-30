58e3506c441e623ab2c87e121aa47550b989726e
William Clark <bclark@kinvey.com>
2016-05-30 18:14:19 -0500

    Style cleanup, remove extraneous function

--- src/newcomp/handler.clj
+++ src/newcomp/handler.clj
@@ -15,81 +15,75 @@
 			                    ByteArrayInputStream)))
 			
 
-	 (defn seq-of-nodes
-	 	[func n init]
-	 	(take n
-	 		(map func (iterate inc init)) 
-	 		)
-	 	)
-	 (defn series-gen
-	 	[func n init]
-	 	(reduce + (seq-of-nodes func n init))
-	 	)
+(defn seq-of-nodes
+	[func n init]
+	(take n
+		(map func (iterate inc init)) 
+		)
+	)
+
+ ; This is one node in the Euler infinite series representing Pi
+ (defn leibniz-node [n] 	
+ 	(let [
+ 			oddnum (+ (* 2.0 n) 1.0)
+ 			signfactor (- 1 (* 2 (mod n 2)))
+ 		]
+ 		(/ (* 4.0 signfactor) oddnum)
+ 	)
+ )
+
+; This is one node in the Leibniz infinite series representing Pi
+(defn euler-node [n]  
+	(/ 6.0 (* n n)	
+	))
 
 
+(defn series-euler
+	[n]
+	(reduce + (seq-of-nodes euler-node n 1))
+	)
 
-	 ; This is one node in the Euler infinite series representing Pi
-	 (defn leibniz-node [n] 	
-	 	(let [
-	 			oddnum (+ (* 2.0 n) 1.0)
-	 			signfactor (- 1 (* 2 (mod n 2)))
-	 		]
-	 		(/ (* 4.0 signfactor) oddnum)
-	 	)
-	 )
-
-	 ; This is one node in the Leibniz infinite series representing Pi
-	 (defn euler-node [n]  
-	 	(/ 6.0 (* n n)	
-	 	))
-
-
-	 (defn series-euler
-	 	[n]
-	 	(reduce + (seq-of-nodes euler-node n 1))
-	 	)
-
-	 (defn series-leibniz
-	 	[n]
-	 	(reduce + (seq-of-nodes leibniz-node n 0))
-	 	)
-
-
-
-	(defn my-euler-dataset [n init]
-		(take n ( map #(sqrt (series-euler %)) (iterate inc init)))
+(defn series-leibniz
+	[n]
+	(reduce + (seq-of-nodes leibniz-node n 0))
 	)
 
 
-	(defn my-leibniz-dataset [n init]
-		(take n ( map series-leibniz (iterate inc init)))
-	)	
+
+(defn my-euler-dataset [n init]
+	(take n ( map #(sqrt (series-euler %)) (iterate inc init)))
+)
 
 
-	(defn gen-png
-	  [dataset size-str init-str]
-	    (let [ init (Integer/parseInt init-str)
-			size (if (nil? size-str)
-	                 100
-	                 (Integer/parseInt size-str))
-			chart (scatter-plot (take size (iterate inc init)) (dataset size init) :x-label "N" :y-label "Estimate" )
-			out-stream (ByteArrayOutputStream.)
-			    in-stream (do
-			                (save chart out-stream)
-			                (ByteArrayInputStream.
-			                  (.toByteArray out-stream)))
-							  ]
-		  	    		    {:body in-stream
-		  						:status 200
-		  	    		            :headers {"Content-Type" "image/png"}}
+(defn my-leibniz-dataset [n init]
+	(take n ( map series-leibniz (iterate inc init)))
+)	
 
-									))
+
+(defn gen-png
+  [dataset size-str init-str]
+    (let [ init (Integer/parseInt init-str)
+		size (if (nil? size-str)
+                 100
+                 (Integer/parseInt size-str))
+		chart (scatter-plot (take size (iterate inc init)) (dataset size init) :x-label "N" :y-label "Estimate" )
+		out-stream (ByteArrayOutputStream.)
+		    in-stream (do
+		                (save chart out-stream)
+		                (ByteArrayInputStream.
+		                  (.toByteArray out-stream)))
+						  ]
+	  	    		    {:body in-stream
+	  						:status 200
+	  	    		            :headers {"Content-Type" "image/png"}}
+
+								))
 				 
 
-	(defn json-response [data & [status]]
-	  {:status  (or status 200)
-	   :headers {"Content-Type" "application/hal+json; charset=utf-8"}
-	   :body    (json/generate-string data)})
+(defn json-response [data & [status]]
+  {:status  (or status 200)
+   :headers {"Content-Type" "application/hal+json; charset=utf-8"}
+   :body    (json/generate-string data)})
 
 
 
@@ -99,8 +93,7 @@
 	(let [size-str (get (:params req) :size "1000")
 	  start-str (get (:params req) :start "1")
 	  func-kw (keyword (get (:params req) :func "leibniz")) ; leibniz is default
-	  my-func (func-kw {:leibniz my-leibniz-dataset :euler my-euler-dataset})
-       ]
+	  my-func (func-kw {:leibniz my-leibniz-dataset :euler my-euler-dataset}) ]
 	   	(json-response (my-func (Integer/parseInt size-str) (Integer/parseInt start-str)))
 			  )
   )
